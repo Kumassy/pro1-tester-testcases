933f91577ef9dc5014102171c6af16c6cd8e7525
Kumassy <kumassyii@gmail.com>
2016-06-19 15:37:38 +0100

    remove lib/pro1/**/* from git stage

--- lib/pro1/tester.rb
+++
@@ -1,7 +0,0 @@
-require "pro1/tester/version"
-
-module Pro1
-  module Tester
-    # Your code goes here...
-  end
-end
--- lib/pro1/tester/version.rb
+++
@@ -1,5 +0,0 @@
-module Pro1
-  module Tester
-    VERSION = "0.1.0"
-  end
-end
